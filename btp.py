3bdd8396ec02bb5ed14720200a0e9c71e22db5b0
kaustubhhiware <hiwarekaustubh@googlemail.com>
2016-11-16 19:56:04 +0530

    New constraint of 4 under prof

--- btp.py
+++ btp.py
@@ -4,6 +4,8 @@ from prettytable import PrettyTable
 import datetime
 import requests
 
+max_per_prof = 4
+
 def download_file():
     response = requests.get("https://docs.google.com/spreadsheets/d/1waz0KHy6CiuNNqzk4uo65bGSbXS5_fJHbHs5p5SPYUQ/export?format=csv&id=1waz0KHy6CiuNNqzk4uo65bGSbXS5_fJHbHs5p5SPYUQ");
     assert response.status_code == 200, 'Wrong status code'
@@ -55,17 +57,18 @@ def listFromcsv(filename):
 				lister[i].append("")
 			#print row[1],len(row)
 			while(1):
+				if j==22:		# upto 20 choices
+					break
 				if row[j]=='' :# last choice
 					break
 				if row[j][0]=='/':# comments ignored
 					break
 				if row[j][0]==' ':# because there exist dumbf#cks
 					break
-				row[j] = row[j].strip()
+				row[j] = row[j].strip() # row[j] is j-1 th preference of ith student
 				lister[i].append(row[j])
 				# print row[j] # j-1 th choice
 				j += 1
-
 			i += 1
 
 	# print lister
@@ -77,40 +80,56 @@ def dicttFromcsv(filename):
 	with open(filename, mode='rb') as infile:
 		reader = csv.reader(infile)
 		d = d = defaultdict(list)
+		prof = dict()
 		for row in reader:
 			if row[1]=="Code" or row[1]=="":
 				continue
 
+
 			d[row[1]].append(int(row[7]))
 			d[row[1]].append(row[8])
 			d[row[1]].append(0)
+			d[row[1]].append(row[3])
+			prof[row[3]] = 0	# new restriction :
+								# each prof can have 4 students with CG > 7
 			#d[row[1]]=list(int(row[7]),row[8],0)
 			# row 1 is code , 7 is capacity , 8 is type of student
 			# 0 is the filled value to be used later
-	return d
+
+	# print "Profs"
+	# print prof
+	return d,prof
 
 
-def showRemaining(projects):
+def showRemaining(projects,profs):
 	"""
 		See left courses capacity filled and total
 	"""
-	pros = PrettyTable(['Code','Capacity','Filled'])
+	pros = PrettyTable(['Code','Capacity','Filled','Prof filled','Prof available'])
 	rem = 0
+	profav = 0
 	for each in sorted(projects.keys()):
 
-		if projects[each][0]!=projects[each][2]:
-			pros.add_row([each,projects[each][0],projects[each][2]])
+		if projects[each][0]!=projects[each][2]:	# capacity not full
+			profFilled = profs[projects[each][3]]
+			if profFilled == max_per_prof:
+				pros.add_row([each,projects[each][0],projects[each][2],profFilled,"X NO"])
+			else:
+				pros.add_row([each,projects[each][0],projects[each][2],profFilled,"  Yes"])
+				profav += 1
 			rem += 1
 
 	print "\nOverview of remaining projects : "
 	print(pros)
 	print rem,"projects remain"
+	print profav,"projects remain with available professors"
 
 
-def allot(projects,people,details):
+def allot(projects,people,profs,details):
 	"""
 		Print DR# wise project alloted
 		details matlab print small details for allocation
+		profs has the count for prof . At max 4 per prof for CG > 7
 	"""
 
 	statsTable = PrettyTable(['Name','Alloted'])
@@ -127,7 +146,7 @@ def allot(projects,people,details):
 				statsTable.add_row([student[0],"Fill choices"])
 				break
 			elif student[i] not in projects: 
-				statsTable.add_row([student[0],""])
+				statsTable.add_row([student[0],"Invalid choice"+str(i)])
 				break
 
 			thisProject = projects[student[i]]
@@ -136,7 +155,6 @@ def allot(projects,people,details):
 			if thisProject[2] == thisProject[0]: # capacity full
 				
 				# remove below for debugging
-				
 				if(details=="1"):
 					print student[i],"full\t",
 				if i==len(student)-1:
@@ -145,11 +163,23 @@ def allot(projects,people,details):
 					statsTable.add_row([student[0],"Out of choices"])
 				continue # look for next 
 		
+			# new constraint : at max max_per_prof people under that prof
+			if profs[thisProject[3]]==max_per_prof:
+				# remove below for debugging
+				if(details=="1"):
+					print student[i],"unavailable since prof already has 4 students\t",
+				if i==len(student)-1:
+					if(details=="1"):
+							print "Out of choices"
+					statsTable.add_row([student[0],"Out of choices(prof filled)"])
+				continue # look for next 
+
 			# if here , it means student gets it
 			if(details=="1"):
 				print student[i],"alloted"
 			statsTable.add_row([student[0],student[i]])
 			projects[student[i]][2] += 1 # update filled positions
+			profs[projects[student[i]][3]] += 1 # number of students under that prof
 			break
 		
 	# tabulated results are printed
@@ -167,10 +197,11 @@ def main():
 
         now = datetime.datetime.now()
 	print "Script started on "+now.strftime("%A %d %B %Y %I:%M:%S %p %Z")
-	#deprecated for the time being
-	details = 0
+	#deprecated for the time being change 0 to 1 for detailed view
+	details = "0"
+
 	#details = raw_input("Print allocation details ?\n 1 for yes , any other char for no : ")
-	projects = dicttFromcsv("Projects.csv")
+	projects,profs = dicttFromcsv("Projects.csv")
 	
 	#assumed that Choices.csv is sorted by DR#
 	# ignoring constraints like Any, BTech , Dual , CG > 8.5 for now
@@ -178,11 +209,11 @@ def main():
 	people = listFromcsv("Choices.csv")
 
 	#print len(people)
-	allot(projects,people,details)
+	allot(projects,people,profs,details)
 
 	#remainder = raw_input("Show remaining options ?\n 1 for yes , any other char for no : ")
 	#if remainder=="1":
-	showRemaining(projects)
+	showRemaining(projects,profs)
 	now = datetime.datetime.now()
 	print "Script ended on "+now.strftime("%A %d %B %Y %I:%M:%S %p %Z")
 
